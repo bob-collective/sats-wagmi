bfea139fe02b89180159984381f1af290ab4f2c5
Slava <slava@interlay.io>
2024-11-25 20:53:18 +0300

    chore: update for the last version of sdk

--- packages/sats-wagmi/src/hooks/useBalance.tsx
+++ packages/sats-wagmi/src/hooks/useBalance.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { UseQueryOptions, useQuery } from '@tanstack/react-query';
-import { EsploraClient, OrdinalsClient, OutPoint } from '@gobob/bob-sdk';
-import { AddressType, getAddressInfo } from 'bitcoin-address-validation';
+import { getBalance } from '@gobob/bob-sdk';
 
 import { useSatsWagmi } from '../provider';
 import { INTERVAL } from '../utils';
@@ -24,56 +23,7 @@ const useBalance = (props: UseBalanceProps = {}) => {
     enabled: Boolean(address),
     queryKey: ['sats-balance', network, address],
     refetchInterval: INTERVAL.SECONDS_30,
-    queryFn: async () => {
-      if (!address) {
-        return { confirmed: BigInt(0), unconfirmed: BigInt(0), total: BigInt(0) };
-      }
-
-      const esploraClient = new EsploraClient(network);
-      const ordinalsClient = new OrdinalsClient(network);
-
-      const addressInfo = getAddressInfo(address);
-
-      if (addressInfo.type === AddressType.p2tr) {
-        const [outputs, cardinalOutputs] = await Promise.all([
-          esploraClient.getAddressUtxos(address),
-          // cardinal = return UTXOs not containing inscriptions or runes
-          ordinalsClient.getOutputsFromAddress(address, 'cardinal')
-        ]);
-
-        const cardinalOutputsSet = new Set(cardinalOutputs.map((output) => output.outpoint));
-
-        const total = outputs.reduce((acc, output) => {
-          if (cardinalOutputsSet.has(OutPoint.toString(output))) {
-            return acc + output.value;
-          }
-
-          return acc;
-        }, 0);
-
-        const confirmed = outputs.reduce((acc, output) => {
-          if (cardinalOutputsSet.has(OutPoint.toString(output)) && output.confirmed) {
-            return acc + output.value;
-          }
-
-          return acc;
-        }, 0);
-
-        return {
-          confirmed: BigInt(confirmed),
-          unconfirmed: BigInt(total - confirmed),
-          total: BigInt(total)
-        };
-      } else {
-        const { confirmed, unconfirmed, total } = await esploraClient.getBalance(address);
-
-        return {
-          confirmed: BigInt(confirmed),
-          unconfirmed: BigInt(unconfirmed),
-          total: BigInt(total)
-        };
-      }
-    },
+    queryFn: () => getBalance(address),
     ...props
   });
 };
