daf7ff9c2154e99b7ce9de230cc19344170ffb45
Rui Simão <rui.daniel.simao@gmail.com>
2025-06-02 10:32:09 +0100

    feat: final

--- packages/sats-wagmi/src/connectors/leap.ts
+++ packages/sats-wagmi/src/connectors/leap.ts
@@ -1,6 +1,6 @@
 import { Network } from 'bitcoin-address-validation';
 
-import { PsbtInputAccounts, SatsConnector } from './base';
+import { SatsConnector } from './base';
 
 declare global {
   interface Window {
@@ -11,7 +11,7 @@ declare global {
 
 class LeapConnector extends SatsConnector {
   constructor(network: Network) {
-    super(network, 'leap', 'Leap', 'https://www.leapwallet.io/');
+    super(network, 'leap', 'Leap Wallet', 'https://www.leapwallet.io/');
   }
 
   async connect(): Promise<void> {
@@ -50,44 +50,16 @@ class LeapConnector extends SatsConnector {
     return window.leapBitcoin.sendBitcoin(toAddress, amount);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async signPsbt(psbtHex: string, psbtInputAccounts: PsbtInputAccounts[]): Promise<string> {
+  async signPsbt(psbtHex: string): Promise<string> {
     if (!this.publicKey) {
       throw new Error('Something went wrong while connecting');
     }
-
-    // // Extract all inputs to be signed
-    // let inputs: number[] = [];
-
-    // for (const input of psbtInputAccounts) {
-    //   for (const index of input.signingIndexes) {
-    //     inputs.push(index);
-    //   }
-    // }
 
     if (!this.paymentAddress) {
       throw new Error('No payment address specified');
     }
 
-    // // Map psbtInputAccounts to SignPsbtOptions for Wallet B's API
-    // const options = {
-    //   autoFinalized: false, // Default value; adjust as needed
-    //   contracts: psbtInputAccounts.map((account) => ({
-    //     id: account.address, // Use address as a unique identifier for the contract
-    //     params: {
-    //       signingIndexes: account.signingIndexes // Pass signing indices, though Wallet B may ignore this
-    //     }
-    //   }))
-    // };
-
-    // // @ts-ignore
-    // return await window.leapBitcoin.request({
-    //   method: 'btc_signPsbt',
-    //   params: [psbtHex, options]
-    // });
-
-    // MEMO: leap wallet only accepts an argument
-    return window.leapBitcoin.signPsbt(psbtHex);
+    return window.leapBitcoin.signPsbt(psbtHex, { autoFinalized: false });
   }
 }
 
